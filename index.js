13eb23c0b343aa91dff09f134050d36238a5dff9
AaronKhah92 <aaron.k.ahmadi@gmail.com>
2022-12-18 19:40:35 +0100

    Fix 6

--- index.js
+++ index.js
@@ -1,53 +1,36 @@
-import { load } from "cheerio";
-const form = document.getElementById("bookmark-form");
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+import express from "express";
+import bodyParser from "body-parser";
+const { json } = bodyParser;
+import cors from "cors";
 
-  const bookmarksFile = document.getElementById("bookmarks-file").files[0];
-  const searchTerm = document.getElementById("search-term").value;
+const app = express();
 
-  // Read contents of bookmarks file
-  const fileReader = new FileReader();
-  fileReader.readAsText(bookmarksFile);
-  const bookmarksHTML = await new Promise((resolve, reject) => {
-    fileReader.onload = () => resolve(fileReader.result);
-    fileReader.onerror = (error) => reject(error);
+app.use(
+  json(),
+  cors({
+    origin: "*",
+  })
+);
+
+app.post("/create-bookmarks-file", async (req, res) => {
+  const bookmarks = req.body.bookmarks;
+  let bookmarksHTML = `<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<!-- This is an automatically generated file.
+     It will be read and overwritten.
+     DO NOT EDIT! -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Bookmarks</TITLE>
+<H1>Bookmarks</H1>
+<DL><p>`;
+
+  bookmarks.forEach(({ url, title }) => {
+    bookmarksHTML += `<DT><A HREF="${url}" ADD_DATE="">${title}</A>\n`;
   });
 
-  // Parse bookmarks HTML
-  const $ = load(bookmarksHTML);
-  const bookmarks = [];
-  $("a").each((i, element) => {
-    const url = $(element).attr("href");
-    const title = $(element).text();
-    bookmarks.push({ url, title });
-  });
-
-  // Filter bookmarks by search term
-  const filteredBookmarks = bookmarks.filter(({ title }) =>
-    title.includes(searchTerm)
-  );
-
-  // Send filtered bookmarks to server to create new bookmarks file
-  const response = await fetch(
-    `${window.location.origin}:3399/create-bookmarks-file`,
-    {
-      method: "POST",
-      body: JSON.stringify({ bookmarks: filteredBookmarks }),
-      headers: { "Content-Type": "application/json" },
-    }
-  );
-
-  console.log(response);
-
-  // Download new bookmarks file
-  const file = await response.blob();
-  const url = window.URL.createObjectURL(file);
-  const a = document.createElement("a");
-  a.style.display = "none";
-  a.href = url;
-  a.download = "filtered-bookmarks.html";
-  document.body.appendChild(a);
-  a.click();
-  window.URL.revokeObjectURL(url);
+  bookmarksHTML += "</DL><p>";
+  const file = new Blob([bookmarksHTML], { type: "text/html" });
+  const text = await file.text();
+  res.send(text);
 });
+
+app.listen(3399, () => console.log("Server listening on port 3399"));
