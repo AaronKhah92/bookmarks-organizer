2e892eb187c60184161cf8744bff7df07fe8e405
AaronKhah92 <aaron.k.ahmadi@gmail.com>
2022-12-18 22:04:06 +0100

    Reverted to commit before vercel mess

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 
 export default {
-  input: "index.js",
+  input: "app.js",
   output: {
     file: "bundle.js",
     format: "iife",
