2cecdd4658f58e0075d203e1288c96f1ed3b4201
AaronKhah92 <aaron.k.ahmadi@gmail.com>
2022-12-18 23:26:56 +0100

    fix 4

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,9 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 
 export default {
-  input: "index.js",
+  input: "./client/index.js",
   output: {
-    file: "bundle.js",
+    file: "./client/bundle.js",
     format: "iife",
   },
   plugins: [resolve(), commonjs()],
